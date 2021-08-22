8e2f47841b38a32948502225d05623f344b7e22a
cavanosa <cavanosa@cavanosa.cav>
2021-08-22 19:41:56 +0200

    config server

---
+++ config-service/src/test/java/com/tutorial/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
